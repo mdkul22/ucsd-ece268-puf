4d2c0c69a34de5c927920db8e680494c408f888f
siva sankar <spalania@ucsd.edu>
2019-03-12 12:17:40 -0700

    Modified puf.v for decoder

--- PUF.v
+++ PUF.v
@@ -5,15 +5,21 @@ module PUF #(parameter N = 4, k = 3)(// k = log2(2N)
 	output wire [2*N-1:0]response
    );
 
-	wire [N-1:0] tune_top, tune_bottom;
+	wire [2*N-1:0] tune_top, tune_bottom;
 
 	(* KEEP_HIERARCHY = "TRUE" *)
 	// change this decoder to basically add them to each PDL.
-	Decoder #(N, k) Decoder(
+	genvar p;
+	generate
+	for (p=2*N-1;p>=0;p=p-1)
+	begin:decoder_id
+	 Decoder #(16,5) Decoder(
     .tune_level(tune_level),
-    .top(tune_top),
-    .bottom(tune_bottom)
+    .top(tune_top[p]),
+    .bottom(tune_bottom[p])
     );
+	end
+	endgenerate
 
 	wire [2*N-1:0] out_top, out_bottom;
 	Multiplier #(N) topMult(
@@ -29,21 +35,20 @@ module PUF #(parameter N = 4, k = 3)(// k = log2(2N)
 		);
 
 	wire [2*N-1:0]topw, bottomw;
-	genvar p;
 	// We generate the blocks required to get MAC arbitration
 	// Modified PUF_unit only has the PDL coarse for tuning.
 	generate
-	for(p = 2*N-1; p>=0; p = k - 1)
+	for(p = 2*N-1; p>=0; p = p - 1)
 	begin: PUF_id
 	PUF_unit #(N) top(
     .in(out_top[p]),
-		.tune(tune_top),
+		.tune(tune_top[p]),
     .out(topw[p])
     );
 
 	PUF_unit #(N) bottom(
     .in(out_bottom[p]),
-	 .tune(tune_bottom),
+	 .tune(tune_bottom[p]),
     .out(bottomw[p])
     );
 // Xilinx based arbiter
